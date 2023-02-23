87fb82852e68c6efcc07408ddd3f8b3aa00c35c1
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2023-02-24 00:30:30 +0800

    feat: change wallpaper, fonts

--- PeggleClone/PeggleClone/PeggleGame/Controller/MainMenuViewController.swift
+++ PeggleClone/PeggleClone/PeggleGame/Controller/MainMenuViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 class MainMenuViewController: UIViewController {
     @IBOutlet var startButton: UIButton!
     @IBOutlet var levelDesignerButton: UIButton!
+    
+    override func viewDidLoad() {
+        for family: String in UIFont.familyNames
+        {
+            print(family)
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
+    }
 
     @IBAction func startButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: goToChooseLevelViewSegue, sender: self)
